c482ffac097eabe7f79159658b0bc9475272ac8c
DuyDirox <duy.nguyennhat@dirox.net>
2024-06-24 09:38:52 +0700

    up

---
+++ 4.PromisesAndTime/16.js
@@ -0,0 +1,42 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            let timer = setTimeout(() => {
+                clearTimeout(timer)
+                reject("Time Limit Exceeded")
+            }, t)
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer)
+                    resolve(result)
+                })
+                .catch((err) => {
+                    clearTimeout(timer)
+                    reject(err)
+                })
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
+
+// Driver code
+fn = async (a, b) => {
+    await new Promise(res => setTimeout(res, 120));
+    return a + b;
+  }
+inputs = [5,10]
+t = 150
+
+timeLimit(fn, t)(...inputs)
+    .then(console.log)
